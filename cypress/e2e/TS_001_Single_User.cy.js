8ce5430c09abf17d216198f7c7273724e60db25f
Emanuel Zini Casaro <emanuel.zini-casaro@appdirect.com>
2023-06-14 17:29:54 -0300

    created new project

--- cypress/e2e/TS_001_Single_User.cy.js
+++ cypress/e2e/TS_001_Single_User.cy.js
@@ -1,56 +1,52 @@
 /// <reference types = "cypress" />
-import spok from "cy-spok";
+import spok from 'cy-spok'
 
-describe("Get a Single List of a User via API", () => {
-  it("TC_001_Validate get Single List via API", () => {
-    cy.visit("/");
+describe('Get a Single List of a User via API', () => {
+  it('TC_001_Validate get Single List via API', () => {
+    cy.visit('/')
     cy.request({
-      method: "GET",
-      url: "/api/users/2",
+      method: 'GET',
+      url: '/api/users/2',
       failOnStatusCode: false,
     }).then((response) => {
-      // cy.log("Response: " + JSON.stringify(response));
-      expect(response.status).to.eq(200);
-      expect(response.body).to.have.property("data");
-      expect(response.body).to.have.property("support");
-      expect(response.body.data).to.have.property("id", 2);
+      expect(response.status).to.eq(200)
+      expect(response.body).to.have.property('data')
+      expect(response.body).to.have.property('support')
+      expect(response.body.data).to.have.property('id', 2)
       expect(response.body.data).to.have.property(
-        "email",
-        "janet.weaver@reqres.in"
-      );
-      expect(response.body.data).to.have.property("first_name", "Janet");
-      expect(response.body.data).to.have.property("last_name", "Weaver");
+        'email',
+        'janet.weaver@reqres.in',
+      )
+      expect(response.body.data).to.have.property('first_name', 'Janet')
+      expect(response.body.data).to.have.property('last_name', 'Weaver')
       expect(response.body.data).to.have.property(
-        "avatar",
-        "https://reqres.in/img/faces/2-image.jpg"
-      );
-    });
-  });
+        'avatar',
+        'https://reqres.in/img/faces/2-image.jpg',
+      )
+    })
+  })
 
-  it("TC_001_Validate Invalid user id with Single List via API", () => {
-    cy.visit("/");
+  it('TC_001_Validate Invalid user id with Single List via API', () => {
+    cy.visit('/')
     cy.request({
-      method: "GET",
-      url: "/api/users/23",
+      method: 'GET',
+      url: '/api/users/23',
       failOnStatusCode: false,
     }).then((response) => {
-      // cy.log("Response: " + JSON.stringify(response));
-      expect(response.status).to.eq(404);
-      expect(response.body).to.be.empty;
-    });
-  });
-});
+      expect(response.status).to.eq(404)
+      expect(response.body).to.be.empty
+    })
+  })
+})
 
 // Case 2: VALIDATING PROPERTIES AND VALUES with INTERCEPT via API
-// https://github.com/bahmutov/cy-spok  ¡Validate ANY object with spock!
-// https://www.youtube.com/watch?v=MLDsqBd_gVU
-describe("Validate and processes the intercep object", () => {
-  it.only("TC_001_Validate get Single List via API", () => {
-    cy.intercept({ method: "GET", url: "/api/users?page=2" }).as("listUsers");
+describe('Validate and processes the intercep object', () => {
+  it('TC_001_Validate get Single List via API', () => {
+    cy.intercept({ method: 'GET', url: '/api/users?page=2' }).as('listUsers')
 
-    cy.visit("/");
-    cy.wait("@listUsers")
-      .its("response")
+    cy.visit('/')
+    cy.wait('@listUsers')
+      .its('response')
       .should(
         spok({
           statusCode: 200,
@@ -65,14 +61,14 @@ describe("Validate and processes the intercep object", () => {
               },
               {
                 id: 8,
-                email: "lindsay.ferguson@reqres.in",
-                first_name: "Lindsay",
-                last_name: spok.startsWith("Fer"), // "Ferguson",
-                avatar: "https://reqres.in/img/faces/8-image.jpg",
+                email: 'lindsay.ferguson@reqres.in',
+                first_name: 'Lindsay',
+                last_name: spok.startsWith('Fer'), // "Ferguson",
+                avatar: 'https://reqres.in/img/faces/8-image.jpg',
               },
             ],
           },
-        })
-      );
-  });
-});
+        }),
+      )
+  })
+})
